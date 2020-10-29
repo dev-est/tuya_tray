dc6c447a08d57fc898dab42fd235d036bdf5ddf1
Dev <devrishi.sagar@gmail.com>
2020-10-29 00:57:40 +0000

    added all lights functionality + implementing OOP for future mods

--- tuya-tray.py
+++ tuya-tray.py
@@ -7,42 +7,56 @@ import time
 from functools import partial
 api = TuyaApi()
 
-def turn_on(device):
-    return device.turn_on()
-def turn_off(device):
-    return device.turn_off()
+class tray():
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    
+    def turn_off(self,device):
+        is_list = isinstance(device,list)
+        if is_list == False:
+            return device.turn_off()
+        else:
+            return [i.turn_off() for i in device]
 
-def main():
-    api.init('LOGIN','PASSWORD',"44","tuya")
-    device_id = api.get_all_devices()
-    devices = dict((i.name(),i) for i in device_id if i.obj_type == 'light' or i.obj_type == 'switch')
+    def turn_on(self,device):
+        is_list = isinstance(device,list)
+        if is_list == False:
+            return device.turn_on()
+        else:
+            return[ i.turn_on() for i in device] 
+    
+    def initUI(self):
+        self.app = QApplication(sys.argv)
+        api.init('LOGIN','PASSWORD',"44","tuya")
+        self.device_ids = api.get_all_devices()
+        self.devices = dict((i.name(),i) for i in self.device_ids if i.obj_type == 'light' or i.obj_type == 'switch')
+        self.lights = dict((i.name(),i) for i in self.device_ids if i.obj_type == 'light')
+        self.devices['All Lights'] = list(self.lights.values())
+        self.tray_icon = QSystemTrayIcon(QIcon('lightbulb.png'),parent=self.app)
+        self.tray_icon.setToolTip('Tuya - Lights')
+        self.menu = QMenu()
+        self.menus = dict()
+        self.buttons = dict()
 
-    app = QApplication(sys.argv)
+        for j in self.devices.keys():
+            self.menus[f"{j}_Action"] = self.menu.addMenu(j)
+            self.menus[f"{j}_Action"].addAction('On')
+            self.menus[f"{j}_Action"].addAction('Off')
+            self.buttons = self.menus[f"{j}_Action"].actions()
+            for i in self.buttons:
+                if i.iconText() == 'On':
+                    i.triggered.connect(partial(self.turn_on,self.devices[j]))
+                elif i.iconText() == 'Off':
+                    i.triggered.connect(partial(self.turn_off,self.devices[j]))
 
-    tray_icon = QSystemTrayIcon(QIcon('lightbulb.png'),parent=app)
-
-    tray_icon.setToolTip('Tuya - Lights')
-    tray_icon.show()
-    menu = QMenu()
-
-    menus = dict()
-    for j in (devices.keys()):
-        menus[f"{j}_Action"] = menu.addMenu(j)
-        menus[f"{j}_Action"].addAction('On')
-        menus[f"{j}_Action"].addAction('Off')
-        buttons = menus[f"{j}_Action"].actions()
-        for i in buttons:
-            if i.iconText() == 'On':
-                i.triggered.connect(partial(turn_on,devices[j]))
-            elif i.iconText() == 'Off':
-                i.triggered.connect(partial(turn_off,devices[j]))
-
-    exitaction = menu.addAction('Exit')
-    exitaction.triggered.connect(app.quit)
-    tray_icon.setContextMenu(menu)
-    app.exec_()
-    sys.exit(app.exec_())
+        self.exitaction = self.menu.addAction('Exit')
+        self.exitaction.triggered.connect(self.app.quit)
+        self.tray_icon.setContextMenu(self.menu)
+        self.tray_icon.show()
+        self.app.exec_()
+        sys.exit(self.app.exec_())
 
 if __name__ == "__main__":
-    while True:
-        main()
+    app = tray()

f787e61bfb5b44e76d7bc9d788fb3859a5c68c95
Dev <devrishi.sagar@gmail.com>
2020-10-31 15:20:07 +0000

    alphabetical grouping + divider for types

--- tuya-tray.py
+++ tuya-tray.py
@@ -25,22 +25,27 @@ class tray():
         if is_list == False:
             return device.turn_on()
         else:
-            return[ i.turn_on() for i in device] 
+            return [i.turn_on() for i in device] 
     
     def initUI(self):
         self.app = QApplication(sys.argv)
         api.init('LOGIN','PASSWORD',"44","tuya")
         self.device_ids = api.get_all_devices()
-        self.devices = dict((i.name(),i) for i in self.device_ids if i.obj_type == 'light' or i.obj_type == 'switch')
-        self.lights = dict((i.name(),i) for i in self.device_ids if i.obj_type == 'light')
+        self.switch = dict(sorted(dict((i.name(),i) for i in self.device_ids if i.obj_type == 'switch').items()))
+        self.lights = dict(sorted(dict((i.name(),i) for i in self.device_ids if i.obj_type == 'light').items()))
+        self.devices = {**self.switch, **self.lights}
         self.devices['All Lights'] = list(self.lights.values())
         self.tray_icon = QSystemTrayIcon(QIcon('lightbulb.png'),parent=self.app)
         self.tray_icon.setToolTip('Tuya - Lights')
         self.menu = QMenu()
         self.menus = dict()
         self.buttons = dict()
-
+        self.counter = 0
         for j in self.devices.keys():
+            if isinstance(self.devices[j],list) == False and self.devices[j].obj_type == 'light':
+                if self.counter == 0:
+                    self.menu.addSeparator()
+                    self.counter += 1
             self.menus[f"{j}_Action"] = self.menu.addMenu(j)
             self.menus[f"{j}_Action"].addAction('On')
             self.menus[f"{j}_Action"].addAction('Off')
